4de8094867974faef955d859b665547cd7f55f74
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-05-11 15:07:32 -0400

    Se creo el modelo.

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+	public class Tecnicos
+	{
+		[Key]
+		public int TecnicoId { get; set; }
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Error: solo se permiten letras.")]
+		[Required(ErrorMessage = "Nombre obligatorio.")]
+		public string? Nombres { get; set; }
+		[Range(1, float.MaxValue, ErrorMessage = "Error: El sueldo no puede ser menor o igual que 0.")]
+		[Required(ErrorMessage = "Campo obligatorio.")]
+		public decimal? SueldoHora { get; set; }
+	}
+}
