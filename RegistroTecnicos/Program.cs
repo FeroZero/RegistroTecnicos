4915d6d3db8708e06c3eca8480988cf52cb89ead
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-05-11 20:58:20 -0400

    inyecte el service

--- RegistroTecnicos/Program.cs
+++ RegistroTecnicos/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using RegistroTecnicos.Components;
 using RegistroTecnicos.DAL;
+using RegistroTecnicos.Services;
 
 namespace RegistroTecnicos
 {
@@ -17,6 +18,8 @@ namespace RegistroTecnicos
 			var ConStr = builder.Configuration.GetConnectionString("ConStr");
 
 			builder.Services.AddDbContext<Contexto>(Options => Options.UseSqlite(ConStr));
+
+			builder.Services.AddScoped<TecnicosServices>();
 
 			var app = builder.Build();
 
