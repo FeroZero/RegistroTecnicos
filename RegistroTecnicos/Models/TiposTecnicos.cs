eee6f30fda48e4d9ef6f5c9a5050ca67589911c6
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-05-18 17:54:51 -0400

    Se agrego el Modelo TiposTecnicos.

---
+++ RegistroTecnicos/Models/TiposTecnicos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class TiposTecnicos
+    {
+        [Key]
+        public int TipoId { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "solo se permiten letras.")]
+        [Required(ErrorMessage = "Campo obligatorio.")]
+        public string? Descripcion { get; set; }
+    }
+}
