7ab75cc739c640a5e73cc22c338e2958abc03413
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-09-06 01:53:39 -0400

    Se incorporo el Modelo TiposTecnicos al contexto.

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -9,5 +9,6 @@ public class Contexto : DbContext
         : base(Options){ }
 
     public DbSet<Tecnicos> Tecnicos { get; set; }
-    public DbSet<TiposTecnicos> TipoTecnicos { get; set; }
+    public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+
 }
