e213adbe9a744babbc5475f8a1f7340e25b83190
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-09-06 00:07:53 -0400

    Hice el modelo TipoTecnicos.

---
+++ Models/TipoTecnicos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+	public class TipoTecnicos
+	{
+		[Key]
+		public int TipoId { get; set; }
+
+		[Required(ErrorMessage = "Campo Obligatorio.")]
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo letras.")]
+		public string? Descripcion { get; set; }
+	}
+}
