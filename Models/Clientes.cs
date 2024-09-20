563245ab428243bb61e60ee07551c4bd00694834
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-09-20 15:15:39 -0400

    Se creo el modelo clientes.

---
+++ Models/Clientes.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Clientes
+{
+	[Key]
+	public int ClienteId { get; set; }
+
+	[Required]
+	[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage =	"Solo letras.")]
+	public string? Nombres { get; set; }
+
+	[Required]
+	[RegularExpression(@"\d{3}-\d{3}-\s{4}$", ErrorMessage = "Numero de telefono.")]
+	public string? WhatsApp { get; set; }
+}
