190d24de5b811797a6348ecbd2a108d8b5b124cc
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-09-22 03:39:57 -0400

    Se agrego el modelo trabajos.

---
+++ Models/Trabajos.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace RegistroTecnicos.Models
+{
+	public class Trabajos
+	{
+		[Key]
+		public int TrabajoId { get; set; }
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		public DateTime Fecha { get; set; }
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo letras.")]
+		public string Descripcion { get; set; }
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		[Range(1, float.MaxValue, ErrorMessage = "El sueldo debe ser mayor a 0.")]
+		public decimal Monto { get; set; }
+
+
+		[ForeignKey("TecnicoId")]
+		public int TecnicoId { get; set; }
+
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		public Tecnicos Tecnicos { get; set; }
+
+		[ForeignKey("ClienteId")]
+		public int ClienteId {  get; set; }
+
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		public Clientes Clientes { get; set; }
+
+		[ForeignKey("PrioridadId")]
+		public int PrioridadId { get; set; }
+
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		public Prioridades Prioridades { get; set; }
+	}
+}
