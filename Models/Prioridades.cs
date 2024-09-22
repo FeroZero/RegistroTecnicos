16faf94e6eceecdb18791c3da484467d75ac8d4d
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-09-22 03:41:23 -0400

    Se creo el modelo prioridades.

---
+++ Models/Prioridades.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+	public class Prioridades
+	{
+		[Key]
+		public int PrioridadId { get; set; }
+
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo letras.")]
+		public string Descripcion { get; set; }
+
+		[Required(ErrorMessage = "Campo Obligatorio")]
+		public DateTime Tiempo { get; set; } = DateTime.Now;
+	}
+}
