a90e93df54d488a195dc9425e4c28f15f2af50e8
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-10-21 05:49:03 -0400

    Servicio del detalle de trabajos.

---
+++ Services/TrabajosDetalleService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Services
+{
+    public class TrabajosDetalleService(Contexto contexto)
+    {
+        public async Task<List<Trabajos>> Listar(Expression<Func<Trabajos, bool>> criterio)
+        {
+            return await contexto.Trabajos
+                .AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
+    }
+}
